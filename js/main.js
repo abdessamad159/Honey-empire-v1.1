1ee9bce5de4992c455f45323c157c17391bdf91c
Abdessamad <abdessamadguia11@gmail.com>
2025-11-23 11:44:35 +0100

    feat: Implement client-side product display, cart management, filtering, and add new product images.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { store } from './state.js';
 import { products } from './products-data.js';
 import { renderPagination } from './pagination.js';
 function getRandomProductImage() {
-  const imgs = ['images/product-3.jpg', 'images/product-4.jpg'];
+  const imgs = ['images/product-1.jpg', 'images/product-2.jpg', 'images/product-3.jpg', 'images/product-4.jpg', 'images/product-5.jpg', 'images/product-6.jpg', 'images/product-7.jpg', 'images/product-8.jpg', 'images/product-9.jpg', 'images/product-10.jpg', 'images/product-11.jpg'];
   return imgs[Math.floor(Math.random() * imgs.length)];
 }
 
